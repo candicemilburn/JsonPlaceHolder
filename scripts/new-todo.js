8081034adabe23cdaddaa24e7cdb967920e78e0a
Candice Milburn <candicemilburn@hotmail.com>
2024-06-12 09:25:49 -0500

    1st exercise

---
+++ scripts/new-todo.js
@@ -0,0 +1,47 @@
+"use strict"
+
+window.onload = () => {
+
+    console.log("meh")
+
+    const createCommentForm = document.querySelector("#createCommentForm");
+    createCommentForm.addEventListener("submit", createAComment);
+
+}
+
+const createAComment = async (event) => {
+
+    event.preventDefault();
+
+    let formData = new FormData(event.target);
+
+    let formDataAsObject = Object.fromEntries(formData);
+
+    try {
+
+        let response = await fetch("https://jsonplaceholder.typicode.com/todos/", 
+            {
+                method: "POST",
+                headers: { "Content-type": "application/json; charset=UTF-8" },
+                body: JSON.stringify(formDataAsObject)
+            }
+
+        );
+
+        let newComment = await response.json();
+
+        const theResultsDiv = document.getElementById("theResults");
+        theResultsDiv.innerHTML = `
+            <p>User ID: ${newComment.userId}</p>
+            <p>ID: ${newComment.id}</p>
+            <p>Comment: ${newComment.title}</p>
+            <p>Completed?: ${newComment.completed}</p>
+          
+            <p>Your message has posted.</p>
+        `
+        console.log(newComment);
+    } catch (error) {
+        console.log("didn't work!")
+    }
+
+}
